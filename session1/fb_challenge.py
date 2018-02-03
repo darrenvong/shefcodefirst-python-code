a630a9dc98be97d3a329136a1544af9a2d75b2b1
Darren Vong <d.vongy@gmail.com>
2018-02-03 18:11:24 +0000

    Minor order reshuffle on the challenge code

--- session1/fb_challenge.py
+++ session1/fb_challenge.py
@@ -1,4 +1,4 @@
-message_template = "If you plug {} enough, {} will give you lots of awesome stickers!"
 blog = "pawlean.com"
 mitochondria = "Pauline"
+message_template = "If you plug {} enough, {} will give you lots of awesome stickers!"
 Print(message_template.format(blog, mitochondria))
