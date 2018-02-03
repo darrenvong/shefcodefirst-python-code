04d7a32793a8983fc2181bb3d077872e62ad6548
Darren Vong <d.vongy@gmail.com>
2018-02-03 17:53:30 +0000

    Added week 1's challenge

---
+++ session1/fb_challenge.py
@@ -0,0 +1,4 @@
+message_template = "If you plug {} enough, {} will give you lots of awesome stickers!"
+blog = "pawlean.com"
+mitochondria = "Pauline"
+Print(message_template.format(blog, mitochondria))
