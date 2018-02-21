ec9ffa435286850d3b5b8905933c675fa6a4169b
Darren Vong <d.vongy@gmail.com>
2018-02-21 02:45:49 +0000

    Added code that will be used during class in session 4 (Flask part 2)

--- session4/in_class_app/app.py
+++ session4/in_class_app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("my_first_app")
 
 @app.route("/")
 def say_hello():
     return render_template("index.html")
+
+# Notice that the function name is different from above!
+# (say_hello_to rather than *just* say_hello!)
+@app.route("/<name>")
+def say_hello_to(name):
+    return render_template("hello.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+    # request.values is a dictionary holding any
+    # POST request data not already part of the URL
+    data = request.values
+
+    return render_template("feedback.html", form_data=data)
 
 # Passing in "debug=True" as an (keyword) argument to app.run(...) will make your Flask powered
 # server/application refreshes automatically when you make any changes to this file.
