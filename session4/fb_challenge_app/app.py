4008e617bb47964c0d010b98285c86e9ba832e31
Darren Vong <d.vongy@gmail.com>
2019-03-06 23:13:02 +0000

    Updated FB challenge code

--- session4/fb_challenge_app/app.py
+++ session4/fb_challenge_app/app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
+from secrets import token_bytes
 
 app = Flask(__name__)
 
-# Dictionary to hold some data that will last between different requests
-# during the user's visit to the app
-session = {"bad_access": False, "message": ""}
+# A secret key is needed to use sessions. A session is what allows us to store
+# information between each (web) request made by a particular user (such as logged
+# in status on a system), since by default (and by design), each request is
+# independent from each other and so aren't aware of each other.
+# To generate secret key tokens, we can use the handy "token_bytes" function coming
+# from the secrets library which comes with our Python installation
+# (but not by default, so it needs to be imported).
+app.secret_key = token_bytes()
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", bad_access=session["bad_access"],
-                           message=session["message"])
+    return render_template("index.html", bad_access=session.get("bad_access"),
+                           message=session.get("message"))
 
 @app.route("/home")
 def home():
     user_info = request.values
-    if user_info: # Check that you're not directly trying to access the page!
+    if user_info:  # Check that you're not directly trying to access the page!
         # Check that you've typed in a name and a password...
         if user_info.get("name") and user_info.get("password"):
             update_session(bad_access=False, message="")
