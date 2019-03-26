6a6cbdd5aa528e262b67bd1d132aa424d33152da
Darren Vong <d.vongy@gmail.com>
2019-03-26 22:58:33 +0000

    Updated Cat API code to fetch API key from environment variables

--- session5/CatAPI/app.py
+++ session5/CatAPI/app.py
@@ -1,6 +1,9 @@
 import requests
-
+import os
 from flask import Flask, render_template, request
+
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -8,7 +11,7 @@ app = Flask(__name__)
 def index():
     response = requests.get(
         'https://api.thecatapi.com/v1/images/search',
-        headers={'x-api-key': '###API_KEY_HERE###'}
+        headers={'x-api-key': os.getenv('API_KEY')}
     )
     data = response.json()[0]
     print(data)
@@ -22,10 +25,10 @@ def add_favourite():
 
     response = requests.post(
         'https://api.thecatapi.com/v1/favourites',
-        headers={'x-api-key': '###API_KEY_HERE###'},
+        headers={'x-api-key': os.getenv('API_KEY')},
         json={
             'image_id': image_id,
-            'sub_id': 'xviyxa'
+            'sub_id': os.getenv('SUB_ID')
         }
     )
     data = response.json()
@@ -36,7 +39,7 @@ def add_favourite():
 def see_favourites():
     response = requests.get(
         'https://api.thecatapi.com/v1/favourites?limit=3',
-        headers={'x-api-key': '###API_KEY_HERE###'}
+        headers={'x-api-key': os.getenv('API_KEY')}
     )
     data = response.json()
     print(data)
